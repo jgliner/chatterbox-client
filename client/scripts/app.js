afc446058ce159032047ad901efb2c688cc20b9f
franklinshieh <franklinshieh@gmail.com>
2016-01-26 08:54:05 -0800

    Attempts to fix some things

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,18 +19,16 @@ $(document).ready(function(){
     console.log('DISP', incoming, holder);
     incoming = sanitizer(incoming);
     for (var i = 0; i < holder[incoming].length; i++) {
-      $('#chats').prepend(`<p><b>${holder[incoming][i][0]}</b>: ${holder[incoming][i][1]}</p>`);
-    }
+      $('#chats').append(`<p><b>${holder[incoming][i][0]}</b>: ${holder[incoming][i][1]}</p>`);
+    } 
   };
 
   var makeRoomList = function(rooms) {
     $('select').empty();
+    $('.rooms').append(`<option>Choose a room</option>`);
     for (var i = 0; i < rooms.length; i++) {
       if (rooms[i] !== '') {
         $('.rooms').append(`<option value="${rooms[i]}">${rooms[i]}</option>`);
-      }
-      else {
-        $('.rooms').append(`<option value="default">Select a Room...</option>`);
       }
     }
   }
@@ -42,15 +40,15 @@ $(document).ready(function(){
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
       contentType: 'application/json',
-      data: {'order': '-updatedAt'},
+      data: {'order': '-updatedAt', 'limit': 1000},
       success: function (data) {
         holder = {};
 
-        for(let i=0; i<data.results.length; i++) {
+        for(var i=0; i<data.results.length; i++) {
           // add post time
-          let msg = data.results[i].text || '';
-          let username = data.results[i].username || 'Anon';
-          let roomname = data.results[i].roomname || 'Unspecified';
+          var msg = data.results[i].text || ' ';
+          var username = data.results[i].username || 'Anon';
+          var roomname = data.results[i].roomname || 'Unspecified';
           msg = sanitizer(msg);
           username = sanitizer(username);
           roomname = sanitizer(roomname);
@@ -66,41 +64,45 @@ $(document).ready(function(){
       }
     });
   };
-  init()
+  init();
   
   setInterval(function() {
     $('#chats').empty();
     init();
-  }, 5000);
+  }, 10000);
 
   $('select').on('change', function(e) {
     e.preventDefault();
     currentRoom = $(this).val();
+    document.getElementById("currentRoom").innerHTML = currentRoom;
     display($(this).val());
   });
+
+
+  // POST METHODS
+  var message = {
+    username: 'hr38',
+    text: 'yet another message!!!',
+    roomname: '4chan'
+  };
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
+  
 
 });
 
 
-  // // POST METHODS
-
-  // var message = {
-  //   username: 'hr38',
-  //   text: 'extra nice',
-  //   roomname: '4chan'
-  // };
-
-  // $.ajax({
-  //   // This is the url you should use to communicate with the parse API server.
-  //   url: 'https://api.parse.com/1/classes/chatterbox',
-  //   type: 'POST',
-  //   data: JSON.stringify(message),
-  //   contentType: 'application/json',
-  //   success: function (data) {
-  //     console.log('chatterbox: Message sent');
-  //   },
-  //   error: function (data) {
-  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  //     console.error('chatterbox: Failed to send message');
-  //   }
-  // });
