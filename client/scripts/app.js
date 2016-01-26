bdf8a27ffb4c7f73afd0f2258518254324514bac
jgliner <jarrett.gliner@gmail.com>
2016-01-25 18:16:54 -0800

    Fix sanitizer

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,14 +9,15 @@ $(document).ready(function(){
   var sanitizer = function (str) {
     return str.replace('&', '&#38')
       .replace('<', '&#60')
-      .replace('>',  '&#61')
+      .replace('>', '&#61')
       .replace('"', '&#34')
       .replace("'", '&#39')
       .replace("/", '&#92');
   };
 
   var display = function(incoming) {
-    console.log(incoming, holder)
+    console.log('DISP', incoming, holder)
+    incoming = sanitizer(incoming)
     $('#chats').empty();
     for (var i = 0; i < holder[incoming].length; i++) {
       $('#chats').append(`<p><b>${holder[incoming][i][0]}</b>: ${holder[incoming][i][1]}</p>`);
@@ -33,29 +34,6 @@ $(document).ready(function(){
       }
     }
   }
-
-  // POST METHODS
-
-  // var message = {
-  //   username: 'hr38',
-  //   text: 'extra nice',
-  //   roomname: '4chan'
-  // };
-
-  // $.ajax({
-  //   // This is the url you should use to communicate with the parse API server.
-  //   url: 'https://api.parse.com/1/classes/chatterbox',
-  //   type: 'POST',
-  //   data: JSON.stringify(message),
-  //   contentType: 'application/json',
-  //   success: function (data) {
-  //     console.log('chatterbox: Message sent');
-  //   },
-  //   error: function (data) {
-  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  //     console.error('chatterbox: Failed to send message');
-  //   }
-  // });
 
   // GET METHODS
 
@@ -92,3 +70,26 @@ $(document).ready(function(){
 
 });
 
+
+  // POST METHODS
+
+  // var message = {
+  //   username: 'hr38',
+  //   text: 'extra nice',
+  //   roomname: '4chan'
+  // };
+
+  // $.ajax({
+  //   // This is the url you should use to communicate with the parse API server.
+  //   url: 'https://api.parse.com/1/classes/chatterbox',
+  //   type: 'POST',
+  //   data: JSON.stringify(message),
+  //   contentType: 'application/json',
+  //   success: function (data) {
+  //     console.log('chatterbox: Message sent');
+  //   },
+  //   error: function (data) {
+  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+  //     console.error('chatterbox: Failed to send message');
+  //   }
+  // });
