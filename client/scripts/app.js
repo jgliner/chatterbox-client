dea24a5725bb47e01d0a04a04d85b1ebf8a8a977
jgliner <jarrett.gliner@gmail.com>
2016-01-25 20:26:04 -0800

    Refresh messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,27 +4,27 @@ $(document).ready(function(){
   var holder = {};
   // format: [username, msg]
 
-  var currentRoom = '';
+  var currentRoom = 'Unspecified';
   
   var sanitizer = function (str) {
-    return str.replace('&', '&#38')
-      .replace('<', '&#60')
-      .replace('>', '&#61')
-      .replace('"', '&#34')
-      .replace("'", '&#39')
-      .replace("/", '&#92');
+    return str.replace(/\&/, '&#38')
+      .replace(/\</, '&#60')
+      .replace(/\>/, '&#61')
+      .replace(/\"/, '&#34')
+      .replace(/\'/, '&#39')
+      .replace(/\//, '&#92');
   };
 
   var display = function(incoming) {
-    console.log('DISP', incoming, holder)
-    incoming = sanitizer(incoming)
-    $('#chats').empty();
+    console.log('DISP', incoming, holder);
+    incoming = sanitizer(incoming);
     for (var i = 0; i < holder[incoming].length; i++) {
-      $('#chats').append(`<p><b>${holder[incoming][i][0]}</b>: ${holder[incoming][i][1]}</p>`);
+      $('#chats').prepend(`<p><b>${holder[incoming][i][0]}</b>: ${holder[incoming][i][1]}</p>`);
     }
   };
 
   var makeRoomList = function(rooms) {
+    $('select').empty();
     for (var i = 0; i < rooms.length; i++) {
       if (rooms[i] !== '') {
         $('.rooms').append(`<option value="${rooms[i]}">${rooms[i]}</option>`);
@@ -36,42 +36,53 @@ $(document).ready(function(){
   }
 
   // GET METHODS
+  var init = function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      contentType: 'application/json',
+      data: {'order': '-updatedAt'},
+      success: function (data) {
+        holder = {};
 
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    contentType: 'application/json',
-    data: {'order': '-updatedAt'},
-    success: function (data) {
-      for(let i=0; i<data.results.length; i++) {
-        // add post time
-        let msg = data.results[i].text || '';
-        let username = data.results[i].username || 'Anon';
-        let roomname = data.results[i].roomname || '';
-        msg = sanitizer(msg);
-        username = sanitizer(username);
-        roomname = sanitizer(roomname);
-        Array.isArray(holder[roomname]) ? holder[roomname].push([username, msg]) : holder[roomname] = [[username, msg]];
+        for(let i=0; i<data.results.length; i++) {
+          // add post time
+          let msg = data.results[i].text || '';
+          let username = data.results[i].username || 'Anon';
+          let roomname = data.results[i].roomname || 'Unspecified';
+          msg = sanitizer(msg);
+          username = sanitizer(username);
+          roomname = sanitizer(roomname);
+          Array.isArray(holder[roomname]) ? holder[roomname].push([username, msg]) : holder[roomname] = [[username, msg]];
+        }
+        console.log(data)
+        makeRoomList(Object.keys(holder).sort());
+        display(currentRoom);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
       }
-      console.log(data)
-      makeRoomList(Object.keys(holder).sort());
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
+    });
+  };
+  init()
+  
+  setInterval(function() {
+    $('#chats').empty();
+    init();
+  }, 5000);
 
   $('select').on('change', function(e) {
     e.preventDefault();
+    currentRoom = $(this).val();
     display($(this).val());
   });
 
 });
 
 
-  // POST METHODS
+  // // POST METHODS
 
   // var message = {
   //   username: 'hr38',
